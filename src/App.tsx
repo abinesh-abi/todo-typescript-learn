3cf47b47d16dd7230c14fa153498619bf7a3fe80
abi <abinesh1729@gmail.com>
2023-05-22 15:34:16 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,28 @@ import "./App.css";
 import { Todo } from "./model";
 import TodoList from "./components/TodoList";
 
-
 const App: React.FC = () => {
-  const [todo, setTodo] = useState<string>('')
-  const [todos, setTodos] = useState<Todo[]>([])
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault()
-    if(todo){
-      setTodos((state:Todo[])=>[...state,{id:Date.now(),isDone:false,todo}])
-      setTodo('')
+    e.preventDefault();
+    if (todo) {
+      setTodos((state: Todo[]) => [
+        ...state,
+        { id: Date.now(), isDone: false, todo },
+      ]);
+      setTodo("");
     }
-  }
+  };
 
   return (
     <div className="App">
-      <span className="heading">Taskify</span>
+      {/* <span className="heading">Taskify</span> */}
+      <span className="heading">Todo Lists</span>
       <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
       <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
-}
-export default App
+};
+export default App;
